cab2b7576289dec8cf767614a9fede6dc9ba02ee
srakotoniary <sombi.rakotoniary@etud.univ-angers.fr>
2013-01-18 11:38:56 +0100

    depot initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '79076ade4c67d09d09f4240516240d35944c1b1a837973abbdefc43e15a8c8a298e041676056cf9a79b98b16d75ea08159927ba7e401dd521bd5dd647dbb70c4'
