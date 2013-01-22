46fed5da3fa75d4d749ca10bac85d5ea5dd93896
srakotoniary <sombi.rakotoniary@etud.univ-angers.fr>
2013-01-22 13:18:11 +0100

    chapitre 5 et exo

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -26,4 +26,18 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Inscription")  
   end
+  
+   it "devrait avoir le bon lien sur le layout" do
+    visit root_path
+    click_link "Aide"
+    response.should have_selector('title', :content => "Aide")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Accueil"
+    response.should have_selector('title', :content => "Accueil")
+    click_link "S'inscrire !"
+    response.should have_selector('title', :content => "Inscription")
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+   end
 end
