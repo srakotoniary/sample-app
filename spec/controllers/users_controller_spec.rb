cb4b318fea2300da9d8f363036257bbb792e7388
srakotoniary <sombi.rakotoniary@etud.univ-angers.fr>
2013-03-26 15:27:07 +0100

    Modele utilisateur avec mot de passe et page profil

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,41 @@
+# encoding: UTF-8
 require 'spec_helper'
 
 describe UsersController do
  render_views
  
-  describe "GET 'new'" do
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "devrait réussir" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "devrait trouver le bon utilisateur" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+  it "devrait avoir le bon titre" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.nom)
+    end
+
+    it "devrait inclure le nom de l'utilisateur" do
+      get :show, :id => @user
+      response.should have_selector("h1", :content => @user.nom)
+    end
+
+    it "devrait avoir une image de profil" do
+      get :show, :id => @user
+      response.should have_selector("h1>img", :class => "gravatar")
+    end
+  end
+ 
+ describe "GET 'new'" do
     it "devrait reussir" do
       get 'new'
       response.should be_success
